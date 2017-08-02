032040f1fec6fc33cfaad5a7b1f985ef9c4aa661
bshenkd <bshenkd@gmail.com>
2017-08-02 18:11:36 -0400

    first commit

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'axon-patent'
+})
+
+Package.onUse(api => {
+  api.use([
+    'vulcan:core',
+    'vulcan:forms',
+    'vulcan:accounts'
+  ])
+
+  api.mainModule('lib/main.js', 'client')
+
+  api.addAssets(['lib/assets/user-image.png'], ['client'])
+})
